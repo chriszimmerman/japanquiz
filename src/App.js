e1ef1dac3091158c831a6c7e209ccd3f58df48ce
chris@chriszimmerman.net <cjay.zimmerman@gmail.com>
2017-04-17 17:03:59 -0400

    Places quiz header with question kanji into its own component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+var data = [
+  {
+    kanji: '花',
+    hiragana: 'はな',
+    romaji: 'hana',
+    english: 'flower'
+  },
+  {
+    kanji: '本',
+    hiragana: 'ほん',
+    romaji: 'hon',
+    english: 'book'
+  },
+  {
+    kanji: '日',
+    hiragana: 'にち',
+    romaji: 'nichi',
+    english: 'day'
+  },
+  {
+    kanji: '歌',
+    hiragana: 'うた',
+    romaji: 'uta',
+    english: 'song'
+  }
+];
 
 class App extends Component {
   render() {
+    var question = data[Math.floor(Math.random() * data.length)];
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <Question data={question} />
+      </div>
+    );
+  }
+}
+
+class Question extends Component {
+  render() {
+    return (
+      <div className="Quiz-header">
+        <div className="Question">
+          {this.props.data.kanji}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
